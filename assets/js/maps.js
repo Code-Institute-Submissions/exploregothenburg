11b3b9fbc596f2177cf58a9ef1322a7548776475
RubySrivastava <ruby.srivas1984@gmail.com>
2021-01-11 23:52:53 +0000

    change zoom level and lat long

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,27 +1,27 @@
 function initMap() {
     var men = { lat: 57.708870, lng: 11.974560 }; /*Gothenburg*/
     var vra = { lat: 57.571000, lng: 11.785640 }; /*Vrango*/
-    var lis = { lat: 57.695320, lng: 11.990840 }; /*Liseberg*/
+    var lis = { lat: 57.695219, lng: 11.992464 }; /*Liseberg*/
     var bor = { lat: 57.721000, lng: 12.940250 }; /*Boras*/
-    var slo = { lat: 57.686820, lng: 11.935010 }; /*Slottsskogen*/
-    var uni = { lat: 57.696990, lng: 11.986500 }; /*Universeum*/
-    var gom = { lat: 57.696991, lng: 11.986500 }; /*Goteborg Museum*/
-    var bot = { lat: 57.696990, lng: 11.986500 }; /*Botanisa*/
-    var tra = { lat: 57.805490, lng: 12.046590 }; /*Tradgarden*/
-    var mar = { lat: 57.886520, lng: 11.580500 }; /*Marstrand*/
-    var ofc = { lat: 57.696930, lng: 11.947250 }; /*Oscar Fredrik Church*/
-    var ask = { lat: 57.696990, lng: 11.986500 }; /*Askim*/
-    var del = { lat: 57.686450, lng: 12.018280 }; /*Delsjon*/
-    var vat = { lat: 58.039180, lng: 11.986500 }; /*Vattlestugan*/
+    var slo = { lat: 57.683966, lng: 11.943923 }; /*Slottsskogen*/
+    var uni = { lat: 57.695535, lng: 11.989406 }; /*Universeum*/
+    var gom = { lat: 57.706389, lng: 11.963889 }; /*Goteborg Museum*/
+    var bot = { lat: 57.682853, lng: 11.950377 }; /*Botaniska*/
+    var tra = { lat: 57.707174, lng: 11.975132 }; /*Tradgarden*/
+    var mar = { lat: 57.88845, lng: 11.596745 }; /*Marstrand*/
+    var ofc = { lat: 57.696314, lng: 11.948104 }; /*Oscar Fredrik Church*/
+    var ask = { lat: 57.615844, lng: 11.945608 }; /*Askim*/
+    var del = { lat: 57.684722, lng: 12.045833 }; /*Delsjon*/
+    var vat = { lat: 57.813243, lng: 12.052485 }; /*Vattlestugan*/
     var goc = { lat: 58.107690, lng: 12.149440 }; /*Gota Canal*/
-    var bro = { lat: 57.705570, lng: 11.963301 }; /*Brogyllen*/
-    var rod = { lat: 57.879530, lng: 12.021010 }; /*Roda Gelato*/
-    var ava = { lat: 57.696990, lng: 11.986500 }; /*Avalon*/
-    var got = { lat: 57.697479, lng: 11.989051 }; /*Gothia Towers*/
+    var bro = { lat: 57.705544, lng: 11.963371 }; /*Brogyllen*/
+    var rod = { lat: 57.696264, lng: 11.952316 }; /*Rada Gelato*/
+    var ava = { lat: 57.703977, lng: 11.968236 }; /*Avalon*/
+    var got = { lat: 57.697391, lng: 11.989012 }; /*Gothia Towers*/
 
     if (document.getElementById("location").value == 'men') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 3,
+            zoom: 10,
             center: men
         });
         new google.maps.Marker({
@@ -32,18 +32,20 @@ function initMap() {
     }
     else if (document.getElementById("location").value == 'vra') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 13,
             center: vra
         });
         new google.maps.Marker({
             position: vra,
+            content:
+        "<h2>Vrango</h2><p>Saltholmen</p>",
             map,
 
         });
     } 
     else if (document.getElementById("location").value == 'lis') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 13,
             center: lis
         });
         new google.maps.Marker({
@@ -54,7 +56,7 @@ function initMap() {
     }
     else if (document.getElementById("location").value == 'bor') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 13,
             center: bor
         });
         new google.maps.Marker({
@@ -65,7 +67,7 @@ function initMap() {
     }
     else if (document.getElementById("location").value == 'slo') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 15,
             center: slo
         });
         new google.maps.Marker({
@@ -76,7 +78,7 @@ function initMap() {
     }
     else if (document.getElementById("location").value == 'uni') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 17,
             center: uni
         });
         new google.maps.Marker({
@@ -87,7 +89,7 @@ function initMap() {
     }
     else if (document.getElementById("location").value == 'gom') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 15,
             center: gom
         });
         new google.maps.Marker({
@@ -98,7 +100,7 @@ function initMap() {
     }
     else if (document.getElementById("location").value == 'bot') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 15,
             center: bot
         });
         new google.maps.Marker({
@@ -109,7 +111,7 @@ function initMap() {
     }
     else if (document.getElementById("location").value == 'tra') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 15,
             center: tra
         });
         new google.maps.Marker({
@@ -120,7 +122,7 @@ function initMap() {
     }
     else if (document.getElementById("location").value == 'mar') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 13,
             center: mar
         });
         new google.maps.Marker({
@@ -131,7 +133,7 @@ function initMap() {
     }
     else if (document.getElementById("location").value == 'ofc') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 13,
             center: ofc
         });
         new google.maps.Marker({
@@ -142,7 +144,7 @@ function initMap() {
     }
     else if (document.getElementById("location").value == 'ask') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 13,
             center: ask
         });
         new google.maps.Marker({
@@ -153,7 +155,7 @@ function initMap() {
     }
     else if (document.getElementById("location").value == 'del') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 13,
             center: del
         });
         new google.maps.Marker({
@@ -186,7 +188,7 @@ function initMap() {
     }
     else if (document.getElementById("location").value == 'bro') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 18,
             center: bro
         });
         new google.maps.Marker({
@@ -197,7 +199,7 @@ function initMap() {
     }
     else if (document.getElementById("location").value == 'rod') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 18,
             center: rod
         });
         new google.maps.Marker({
@@ -208,7 +210,7 @@ function initMap() {
     }
     else if (document.getElementById("location").value == 'ava') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 18,
             center: ava
         });
         new google.maps.Marker({
@@ -219,7 +221,7 @@ function initMap() {
     }
     else if (document.getElementById("location").value == 'got') {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 10,
+            zoom: 15,
             center: got
         });
         new google.maps.Marker({
