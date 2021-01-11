25ee5c4e51333fadff03d8f900e753379bc96374
RubySrivastava <ruby.srivas1984@gmail.com>
2021-01-11 15:49:37 +0000

    add marker to map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -133,4 +133,24 @@ function initMap() {
             center: got
         });
     }
+    // Create an array of alphabetical characters used to label the markers.
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  // Add some markers to the map.
+  // Note: The code uses the JavaScript Array.prototype.map() method to
+  // create an array of markers based on a given "locations" array.
+  // The map() method here has nothing to do with the Google Maps API.
+  const markers = locations.map((location, i) => {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+    });
+  });
+
+  // Add a marker clusterer to manage the markers.
+  // @ts-ignore MarkerClusterer defined via script
+  /*new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });*/
 }
