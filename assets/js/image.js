7580257b1bcc5d493185fa619b77abc6ff5a83fb
RubySrivastava <ruby.srivas1984@gmail.com>
2021-01-29 13:45:11 +0000

    create file for image lighthouse

--- assets/js/image.js
+++ assets/js/image.js
@@ -1,20 +1,29 @@
-// Get the modal
-var modal = document.getElementsByClassName("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementsByClassName("card-img");
-var modalImg = document.getElementsByClassName("img01");
-var captionText = document.getElementsByClassName("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
 }
+// Close the Modal
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].ClassName.replace("active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
 }
